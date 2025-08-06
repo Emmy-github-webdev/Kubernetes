1c74bdbbf81b8abcb0b1282d087f8839b0f851b0
kramp-emmanuel <emmanuel.ogah@kramp.com>
2025-08-06 09:17:35 +0200

    Optimized multiple EC2 creation

--- 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/ec2.tf
+++ 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/ec2.tf
@@ -14,56 +14,56 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "master" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  key_name                    = var.key-name
-  subnet_id                   = aws_subnet.public-subnet.id
-  vpc_security_group_ids      = [aws_security_group.security-group.id]
-  iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
-
-  tags = {
-    Name = var.master
-  }
-}
-
-resource "aws_instance" "worker1" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  key_name                    = var.key-name
-  subnet_id                   = aws_subnet.public-subnet.id
-  vpc_security_group_ids      = [aws_security_group.security-group.id]
-  iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
-
-  tags = {
-    Name = var.worker1
-  }
-}
-
-resource "aws_instance" "worker2" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  key_name                    = var.key-name
-  subnet_id                   = aws_subnet.public-subnet.id
-  vpc_security_group_ids      = [aws_security_group.security-group.id]
-  iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
-
-  tags = {
-    Name = var.worker2
-  }
-}
-
-# resource "aws_instance" "ec2" {
-#   for_each = local.ec2_instances
-#   ami = data.aws_ami.ubuntu.id
-#   instance_type = "t3.micro"
-#   key_name = var.key-name
-#   subnet_id = aws_subnet.public-subnet.id
-#   vpc_security_group_ids = [aws_security_group.security-group.id]
-#   iam_instance_profile = aws_iam_instance_profile.instance-profile.name
+# resource "aws_instance" "master" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = "t3.micro"
+#   key_name                    = var.key-name
+#   subnet_id                   = aws_subnet.public-subnet.id
+#   vpc_security_group_ids      = [aws_security_group.security-group.id]
+#   iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
 
 #   tags = {
-#     Name = each.value
-#     Role = each.key
+#     Name = var.master
 #   }
 # }
+
+# resource "aws_instance" "worker1" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = "t3.micro"
+#   key_name                    = var.key-name
+#   subnet_id                   = aws_subnet.public-subnet.id
+#   vpc_security_group_ids      = [aws_security_group.security-group.id]
+#   iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
+
+#   tags = {
+#     Name = var.worker1
+#   }
+# }
+
+# resource "aws_instance" "worker2" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = "t3.micro"
+#   key_name                    = var.key-name
+#   subnet_id                   = aws_subnet.public-subnet.id
+#   vpc_security_group_ids      = [aws_security_group.security-group.id]
+#   iam_instance_profile        = aws_iam_instance_profile.instance-profile.name
+
+#   tags = {
+#     Name = var.worker2
+#   }
+# }
+
+resource "aws_instance" "ec2" {
+  for_each = local.ec2_instances
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = var.key-name
+  subnet_id = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.security-group.id]
+  iam_instance_profile = aws_iam_instance_profile.instance-profile.name
+  user_data = templatefile("scripts/setup.sh", {})
+  tags = {
+    Name = each.value
+    Role = each.key
+  }
+}
--- 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/variables.tf
+++ 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/variables.tf
@@ -4,7 +4,7 @@ variable "subnet-name" {}
 variable "rt-name" {}
 variable "sg-name" {}
 variable "key-name" {}
-variable "instance-name" {}
+# variable "instance-name" {}
 variable "iam-role" {}
 variable "master" {}
 variable "worker1" {}
