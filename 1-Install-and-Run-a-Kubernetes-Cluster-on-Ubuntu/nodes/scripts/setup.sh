5a653b5589ba04220ae0e7be71d8d08bea6f8c6b
kramp-emmanuel <emmanuel.ogah@kramp.com>
2025-08-06 09:47:21 +0200

    Disabled swap settings

--- 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/scripts/setup.sh
+++ 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/scripts/setup.sh
@@ -5,3 +5,7 @@ hostnamectl set-hostname ${hostname}
 
 # Update /etc/hosts to reflect the new hostname
 echo "127.0.1.1 ${hostname}" >> /etc/hosts
+
+# Disable the swap
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
