28203951b04fc2cee3b19adb31ca126e640066a6
emmy-github-webdev <ogahemmanuel75@gmail.com>
2025-08-07 11:40:50 +0200

    Updated the script

--- 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/scripts/setup.sh
+++ 1-Install-and-Run-a-Kubernetes-Cluster-on-Ubuntu/nodes/scripts/setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Set hostname
-hostnamectl set-hostname ${hostname}
+#hostnamectl set-hostname ${hostname}
 
 # Update /etc/hosts to reflect the new hostname
-echo "127.0.1.1 ${hostname}" >> /etc/hosts
+#echo "127.0.1.1 ${hostname}" >> /etc/hosts
 
 # Disable the swap settings
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Install Containerd
-cat <<EOF | tee /etc/modules-load.d/containerd.conf
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
 # Load the kernel modules immediately
-modprobe overlay
-modprobe br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
 # Enable the IPv6 settings
-cat <<EOF | tee /etc/sysctl.d/kubernetes.conf
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
@@ -29,32 +29,38 @@ EOF
 
 # Install Kubernetes Components
 # Add Kubernetes Repositories to the Instance
-apt-get update
-curl -fsSL https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-apt update
+sudo apt-get update
+curl -fsSL https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor | sudo tee /etc/apt/keyrings/kubernetes-apt-keyring.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
 
 # Install kubeadm, kubelet, and kubectl
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 # Set Containerd as Default
-mkdir -p /etc/containerd
-containerd config default | tee /etc/containerd/config.toml > /dev/null
-systemctl restart containerd
+sudo mkdir -p /etc/containerd
+sudo apt install -y containerd
+containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+sudo systemctl restart containerd
+systemctl status containerd
+
+
 
 # For only master node
 if [ "${role}" = "master" ]; then
   # Intializing kubernetes Master Node
   echo "Intializing kubernetes Master Node..."
-  kubeadm init --pod-network-cidr=10.244.0.0/16
+  # For testing purpose only
+  sudo kubeadm init --pod-network-cidr=10.244.0.0/16 \
+  --ignore-preflight-errors=Mem,FileContent--proc-sys-net-ipv4-ip_forward
 
   # Setuping kubeconfig for the master node
   echo "Setuping kubeconfig for the master node..."
-  mkdir -p $HOME/.kube
-  cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  chown $(id -u):$(id -g) $HOME/.kube/config
+  sudo mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
   # setup the CNI as flannel for the kubernetes cluster
   echo "setup the CNI as flannel for the kubernetes cluster..."
@@ -62,8 +68,9 @@ if [ "${role}" = "master" ]; then
 
   # Join Worker Nodes
   echo "Join Worker Nodes..."
-  kubeadm join <master-ip>:6443 --token <token> --discovery-token-ca-cert-hash sha256:<hash>
-  
+  # kubeadm join <master-ip>:6443 --token <token> --discovery-token-ca-cert-hash sha256:<hash>
+  # How to get the real value - kubeadm token create --print-join-command
+  kubeadm join 172.31.39.8:6443 --token gkhryh.k55atakut4z7vnd1 --discovery-token-ca-cert-hash sha256:17c3413311de0a6a28ad5c7216b7ae3ce776a560f1d77eefabf899b8e19f1f84
   # Verify the Kubernetes Cluster
   echo "Verify the Kubernetes Cluster..."
   kubectl get nodes
