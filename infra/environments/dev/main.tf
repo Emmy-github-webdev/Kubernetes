a2ac30674d5c19f597e9415fbff46f6df37df9d9
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-05-30 17:49:37 +0200

    Fixed source path

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,12 +1,12 @@
 module "tags" {
-  source = "../../modules/tags"
+  source = "../../modules/terraform-tags"
   env_name = var.env_name
   project_name = var.project_name
   region_name = var.region_name
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "../../modules/network"
   tags = module.tags.tags
   vpc_cidr = "10.0.0.0/16"
 }
