a994201e83d7694c84b7955edcca2249cb7e602f
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-05-30 17:31:20 +0200

    Created VPC

---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,10 @@
+variable "tags" {
+  description = "Global resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
---
+++ infra/modules/network/vpc.tf
@@ -0,0 +1,10 @@
+# VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.tags.project_name}-${var.tags.environment}-vpc"
+  }
+}
