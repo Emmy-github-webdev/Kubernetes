dbd01b2e81a795f1fc2c09844d78709e50151b25
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-02 00:00:40 +0200

    Fixed terraform fomatter issue

--- infra/modules/network/outputs.tf
+++ infra/modules/network/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = aws_vpc.eks_vpc.id
+  value       = aws_vpc.eks_vpc.id
 }
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
-  value = aws_subnet.eks_public_subnets[*].id
+  value       = aws_subnet.eks_public_subnets[*].id
 }
 
 output "private_subnet_ids" {
   description = "List of private subnet IDs"
-  value = aws_subnet.eks_private_subnets[*].id
+  value       = aws_subnet.eks_private_subnets[*].id
 }
