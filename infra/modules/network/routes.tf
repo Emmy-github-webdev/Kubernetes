2b32ac0ec6a37078dcc4f9e8f1b7c5999d523c14
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-02 21:13:05 +0200

    Created routes

---
+++ infra/modules/network/routes.tf
@@ -0,0 +1,24 @@
+resource "aws_route_table" "eks_private_rt" {
+  for_each = local.private_subnets
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "${var.tags.project}-${var.tags.environment}-private-rt-${each.key}"
+  }
+}
+
+# Default Routes to NAT Gateways
+resource "aws_route" "eks_private_nat" {
+  for_each = local.private_subnets
+
+  route_table_id         = aws_route_table.eks_private_rt[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.eks_nat[each.key].id
+}
+
+resource "aws_route_table_association" "eks_private_rt_assoc" {
+  for_each = local.private_subnets
+
+  subnet_id      = aws_subnet.eks_private_subnets[each.key].id
+  route_table_id = aws_route_table.eks_private_rt[each.key].id
+}
