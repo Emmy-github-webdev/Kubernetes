79539c805b68cf40736e60888e401ac9d2565671
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-03 14:36:39 +0200

    Fixed checkov scan error

--- infra/modules/IAM/variables.tf
+++ infra/modules/IAM/variables.tf
@@ -8,3 +8,8 @@ variable "vpc_flow_logs_arn" {
   description = "IAM Role ARN for VPC flow logs"
   type        = string
 }
+
+output "eks_cluster_role_arn" {
+  description = "ARN of the IAM role for EKS cluster"
+  value       = aws_iam_role.eks_cluster_role.arn
+}
--- infra/modules/cloudwatch/outputs.tf
+++ infra/modules/cloudwatch/outputs.tf
@@ -3,8 +3,3 @@ output "vpc_flow_logs_cloudwatch_loggroup_arn" {
   description = "CloudWatch log group for VPC flow logs"
   value       = aws_cloudwatch_log_group.eks_vpc_flow_logs.arn
 }
-
-output "eks_cluster_role_arn" {
-  description = "ARN of the IAM role for EKS cluster"
-  value       = aws_iam_role.eks_cluster_role.arn
-}
