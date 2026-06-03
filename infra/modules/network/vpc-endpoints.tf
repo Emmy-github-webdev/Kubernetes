ef29c7aa41241b11e54ecaf1ded577909285e6d2
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-03 20:35:12 +0200

    Created vpce for interface and gateway

--- infra/modules/network/vpc-endpoints.tf
+++ infra/modules/network/vpc-endpoints.tf
@@ -48,10 +48,11 @@ resource "aws_security_group" "vpce" {
     security_groups = [var.sg_eks_nodes_id]
   }
 
-  # egress {
-  #   protocol    = "-1"
-  #   from_port   = 0
-  #   to_port     = 0
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  egress {
+    description     = "HTTPS to EKS nodes"
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
+    security_groups = [var.sg_eks_nodes_id]
+  }
 }
