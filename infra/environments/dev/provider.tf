134980aeae72c0b6df33835f4691c4be63d3fc27
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-04 23:44:16 +0200

    Troubleshoot access issue

--- infra/environments/dev/provider.tf
+++ infra/environments/dev/provider.tf
@@ -20,11 +20,11 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.eks_cluster_certificate)
-  token                  = data.aws_eks_auth.cluster.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-data "aws_eks_auth" "cluster" {
+data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
