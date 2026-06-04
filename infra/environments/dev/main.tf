e6dacc03939164d29e2d7027213d3983b45961a3
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-04 17:30:22 +0200

    Added launch template

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
   cloudwatch_log_group_arn   = module.cloudwatch.vpc_flow_logs_cloudwatch_loggroup_arn
   vpc_flow_logs_iam_role_arn = module.iam.vpc_flow_logs_iam_role_arn
   cluster_name               = module.eks.cluster_name
-  sg_eks_nodes_id            = module.security_groups.eks_nodes_role_id
+  sg_eks_nodes_id            = module.eks.eks_nodes_role_id
   oidc_issuer_url            = module.eks.oidc_issuer_url
 }
 
