81cc0d5cf05a0fd487b59af8f41882bed3333be6
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-04 17:17:51 +0200

    Added launch template

--- infra/modules/eks/node_group.tf
+++ infra/modules/eks/node_group.tf
@@ -28,7 +28,7 @@ resource "aws_eks_node_group" "eks_managed_node_group" {
 
   launch_template {
     id      = aws_launch_template.eks_nodes.id
-    version = aws_launch_template.eks_nodes.latest_version_number
+    version = "$Latest"
   }
 
   scaling_config {
