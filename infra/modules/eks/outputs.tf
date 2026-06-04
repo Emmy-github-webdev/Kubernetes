785323f55b6541bb8b61c1e26bb202adb61abeaf
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-04 23:56:25 +0200

    Troubleshoot access issue

--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -42,11 +42,3 @@ output "eks_cluster_arn" {
   value       = aws_eks_cluster.eks_cluster.arn
   description = "EKS Cluster ARN"
 }
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
-
-output "eks_cluster_certificate" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-}
