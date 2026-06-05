cdf96b7a2a9bf4d09cb0632788934f5b170f67f1
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-05 17:06:46 +0200

    Fixing access entry key error

--- infra/modules/eks/eks_cluster.tf
+++ infra/modules/eks/eks_cluster.tf
@@ -109,13 +109,13 @@ locals {
 
 resource "aws_eks_access_entry" "github" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = locals.github_role_arn
+  principal_arn = local.github_role_arn
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
-  principal_arn = locals.github_role_arn
+  principal_arn = local.github_role_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
