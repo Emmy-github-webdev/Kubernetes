2127c4b0961429f398bdea2a2ddf5a907046a54a
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-06 19:18:27 +0200

    Testing admin user role

--- infra/modules/eks/eks_cluster.tf
+++ infra/modules/eks/eks_cluster.tf
@@ -61,23 +61,36 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+#########################################################################
+resource "aws_iam_role" "eks_admin" {
+  name = "${var.tags.project}-${var.tags.environment}-admin"
 
-# resource "aws_iam_role" "eks_admin" {
-#   name = "${var.tags.project}-${var.tags.environment}-eks-admin"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+resource "aws_eks_access_policy_association" "eks_admin" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = aws_iam_role.eks_admin.arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+####################################################################
 resource "aws_iam_role" "eks_admin" {
   name = "${var.tags.project}-${var.tags.environment}-admin-role"
 
