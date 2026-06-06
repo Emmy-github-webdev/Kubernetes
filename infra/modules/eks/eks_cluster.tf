b82ebea063c5b2e5d00bc7c7ffe8baaca0e1ff32
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-06 22:03:02 +0200

    Testing admin user role

--- infra/modules/eks/eks_cluster.tf
+++ infra/modules/eks/eks_cluster.tf
@@ -163,7 +163,7 @@ resource "aws_eks_cluster" "eks_cluster" {
     endpoint_private_access = true
     endpoint_public_access  = true
     subnet_ids              = var.private_subnet_ids
-    security_group_ids      = [aws_security_group.eks_cluster.id]
+    # security_group_ids      = [aws_security_group.eks_cluster.id]
   }
 
   # encryption_config {
