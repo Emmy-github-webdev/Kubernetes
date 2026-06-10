c25d96f7374060a4e5c7c8fb4584625e810b90d3
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-10 16:27:37 +0200

    Fixed Tf format error

--- infra/environments/prod/helm.tf
+++ infra/environments/prod/helm.tf
@@ -1,10 +1,10 @@
-resource "helm_release" "ingress_nginx" {
-  name             = "ingress-nginx"
-  namespace        = "ingress-nginx"
-  create_namespace = true
+# resource "helm_release" "ingress_nginx" {
+#   name             = "ingress-nginx"
+#   namespace        = "ingress-nginx"
+#   create_namespace = true
 
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+#   repository = "https://kubernetes.github.io/ingress-nginx"
+#   chart      = "ingress-nginx"
 
-  depends_on = [module.eks]
-}
+#   depends_on = [module.eks]
+# }
