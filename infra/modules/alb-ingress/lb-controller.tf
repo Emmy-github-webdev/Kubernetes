9ae513e910ff08d6180a04fbb9d9b27bf8f8f0cc
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-12 15:06:19 +0200

    Added load balancer controller ingress

--- infra/modules/alb-ingress/lb-controller.tf
+++ infra/modules/alb-ingress/lb-controller.tf
@@ -74,29 +74,31 @@ resource "helm_release" "alb_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
 
-  set = [
-    {
+  set {
       name  = "clusterName"
       value = var.cluster_name
-    },
-    {
+    }
+
+  set {
       name  = "serviceAccount.create"
       value = "false"
-    },
-    {
+  }
+
+  set{
       name  = "serviceAccount.name"
       value = "${var.tags.project}-${var.tags.environment}-lb-controller"
-    },
-    {
+  }
+
+  set {
       name  = "region"
       value = var.tags.region
-    },
-    {
+  }
+
+  set {
       name  = "vpcId"
       value = var.vpc_id
-    }
-  ]
-
+  }
+  
 }
 
 # To be updated with actual application service and ingress resources after the ALB Ingress Controller is deployed and working correctly. This is just a placeholder to ensure the controller is set up properly.
