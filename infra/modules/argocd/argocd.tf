bfbc2f42045081bbe31ef2cc28169bbf6230c529
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-12 22:52:28 +0200

    Added argocd

--- infra/modules/argocd/argocd.tf
+++ infra/modules/argocd/argocd.tf
@@ -2,20 +2,10 @@ resource "helm_release" "argocd" {
   name             = "${var.tags.project}-${var.tags.environment}-argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  namespace        = "${var.tags.project}-${var.tags.environment}-argocd"
+  namespace        = "argocd"
   create_namespace = true
 
   values = [
     file("${path.module}/values.yaml")
-  ]
-}
-
-resource "kubernetes_manifest" "root_app" {
-  manifest = yamldecode(
-    file("${path.module}/root-app.yaml")
-  )
-
-  depends_on = [
-    helm_release.argocd
   ]
 }
