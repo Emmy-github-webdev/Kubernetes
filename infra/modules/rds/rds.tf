fef750bccb2a0d22b77748f423ad5257068d2613
Emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-23 13:01:40 +0000

    Testing postgres provider

--- infra/modules/rds/rds.tf
+++ infra/modules/rds/rds.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     postgresql = {
-      source = "cyrilgdn/postgresql"
+      source  = "cyrilgdn/postgresql"
       version = "~> 1.26"
     }
   }
@@ -186,7 +186,7 @@ resource "aws_elasticache_subnet_group" "redis" {
 
   name = "${var.tags.environment}-redis"
 
-  subnet_ids = "${var.private_subnet_ids}"
+  subnet_ids = "{$var.private_subnet_ids}"
 }
 
 resource "aws_elasticache_replication_group" "redis" {
