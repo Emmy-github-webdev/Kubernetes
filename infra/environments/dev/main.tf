a7bcdcd97748554331f0908f550fc17c80ea1eb3
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-24 22:13:23 +0200

    Added postgres provider

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -81,4 +81,7 @@ module "rds" {
   private_subnet_ids         = module.vpc.private_subnet_ids
   eks_oidc_provider_arn      = module.eks.oidc_provider_arn
   vpc_id                     = module.vpc.vpc_id
+  providers = {
+    postgresql = postgresql
+  }
 }
--- infra/modules/rds/rds.tf
+++ infra/modules/rds/rds.tf
@@ -136,6 +136,14 @@
 
 
 
+terraform {
+  required_providers {
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.26.0"
+    }
+  }
+}
 
 locals {
   services = {
