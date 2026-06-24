aa327eb22b42c34097049c60179d4c41994b23ee
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-24 22:17:02 +0200

    Added postgres provider

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -81,7 +81,4 @@ module "rds" {
   private_subnet_ids         = module.vpc.private_subnet_ids
   eks_oidc_provider_arn      = module.eks.oidc_provider_arn
   vpc_id                     = module.vpc.vpc_id
-  providers = {
-    postgresql = postgresql
-  }
 }
