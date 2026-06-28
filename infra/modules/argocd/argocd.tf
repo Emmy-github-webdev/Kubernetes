db91923af472dfaab1ff0fb986eec1818bc2aeaf
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-28 17:00:38 +0200

    Added argocd root app

--- infra/modules/argocd/argocd.tf
+++ infra/modules/argocd/argocd.tf
@@ -9,3 +9,42 @@ resource "helm_release" "argocd" {
     file("${path.module}/values.yaml")
   ]
 }
+
+resource "kubernetes_manifest" "argocd_root_app" {
+
+  depends_on = [
+    helm_release.argocd
+  ]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+
+    metadata = {
+      name      = "${var.tags.environment}-platform-root"
+      namespace = "argocd"
+    }
+
+    spec = {
+      project = "default"
+
+      source = {
+        repoURL        = "https://github.com/Emmy-github-webdev/Kubernetes-argocd.git"
+        targetRevision = "main"
+        path           = "argocd/${var.tags.environment}"
+      }
+
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "argocd"
+      }
+
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
