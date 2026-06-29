d8d5e53780fee137cc25897a44c862b8d7b922a1
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-06-29 15:59:51 +0200

    Reverted

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -45,7 +45,7 @@ module "eks" {
 }
 
 module "argocd" {
-  source = "../../platform/argocd"
+  source = "../../modules/argocd"
   tags   = module.tags.common_tags
   providers = {
     kubernetes = kubernetes
@@ -55,7 +55,7 @@ module "argocd" {
 }
 
 module "alb_ingress" {
-  source            = "../../platform/alb-ingress"
+  source            = "../../modules/alb-ingress"
   tags              = module.tags.common_tags
   cluster_name      = module.eks.cluster_name
   oidc_issuer_url   = module.eks.oidc_issuer_url
