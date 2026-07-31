30cfdf910d7f1296fdabb09aed8022567f0d218b
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-07-31 16:51:33 +0200

    Fixed velero installtion error

--- infra/modules/argocd/argocd.tf
+++ infra/modules/argocd/argocd.tf
@@ -346,7 +346,7 @@ data "aws_iam_policy_document" "velero_assume_role" {
 }
 
 resource "aws_s3_bucket" "velero" {
-  bucket = "${var.tags.project}-${var.tags.environment}-velero-backups"
+  bucket = "${var.tags.project}-${var.tags.environment}-${data.aws_caller_identity.current.account_id}-velero-backups"
 }
 
 resource "aws_iam_role" "velero" {
